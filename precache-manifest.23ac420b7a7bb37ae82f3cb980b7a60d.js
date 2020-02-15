97f23244548e94b9087c934413894c212d0cb362
rtxrulez <>
2020-02-15 07:37:00 +0300

    Updates

---
+++ precache-manifest.23ac420b7a7bb37ae82f3cb980b7a60d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac689e838ac695e1b73ee2b12a2c594",
+    "url": "/staff/index.html"
+  },
+  {
+    "revision": "daf520c277fb5d6c2aea",
+    "url": "/staff/static/css/main.cdc98494.chunk.css"
+  },
+  {
+    "revision": "0c449d886bc93f81438f",
+    "url": "/staff/static/js/2.04063f3b.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/staff/static/js/2.04063f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf520c277fb5d6c2aea",
+    "url": "/staff/static/js/main.e5d63559.chunk.js"
+  },
+  {
+    "revision": "5c1bdbf8e537a348210d",
+    "url": "/staff/static/js/runtime-main.4f76c176.js"
+  }
+]);
